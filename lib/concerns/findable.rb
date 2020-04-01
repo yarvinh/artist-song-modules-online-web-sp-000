fd9b0d2814c249b9c149ff015aab39a498ab0486
binding-archive-7591 <yarhumher@hotmail.com>
2020-04-01 16:47:59 +0000

    Automatically backed up by Learn

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -2,7 +2,7 @@
 module Memorable
     module InstanceMethods
       def self.find_by_name(name)
-    ClassMethods.all.detect{|a| a.name == name}
+    @@neutral.detect{|a| a.name == name}
       end
   end
 
