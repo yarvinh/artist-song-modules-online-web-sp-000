2bc42b24a97459ba997774b8c37bc0bb49f69667
binding-archive-7591 <yarhumher@hotmail.com>
2020-04-01 16:42:35 +0000

    Automatically backed up by Learn

--- config/environment.rb
+++ config/environment.rb
@@ -7,20 +7,3 @@ require 'pry'
 require_relative '../lib/artist.rb'
 require_relative '../lib/song.rb'
 
-# module Memorable
-#     module InstanceMethods
-#       def to_param
-#         name.downcase.gsub(' ', '-')
-#       end
-#     end
-#
-#     module ClassMethods
-#       def reset_all
-#         self.all.clear
-#       end
-#
-#       def count
-#         self.all.count
-#       end
-#     end
-#   end
